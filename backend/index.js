f63423a204a533088c2d19a0012d819d5a59276b
HuXn WebDev <webdevacademy6@gmail.com>
2023-10-26 08:36:26 -0700

    Setup

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+// packages
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+// Utiles
+import connectDB from "./config/db.js";
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
